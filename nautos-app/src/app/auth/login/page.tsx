a99692d172d6e5f18f1504e88d372e77e15509dc
ritishanegi <ritishanegi19@gmail.com>
2026-06-08 13:10:28 +0530

    next imsge import reoved

--- nautos-app/src/app/auth/login/page.tsx
+++ nautos-app/src/app/auth/login/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Eye, EyeOff } from "lucide-react";
-import Image from "next/image";
+
 
 export default function LoginPage() {
   const router = useRouter();
